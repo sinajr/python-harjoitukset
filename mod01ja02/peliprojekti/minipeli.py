b6602736d00520dec44bf0998d454ab9d7d6c988
sinajr <jamyarisina@gmail.com>
2026-08-28 14:20:19 +0300

    update

--- mod01ja02/peliprojekti/minipeli.py
+++ mod01ja02/peliprojekti/minipeli.py
@@ -4,4 +4,4 @@ playerAge = int(input("player age:"));
 if  18 <= playerAge:
     print("welcome "+ playerName +" "+ "you are"+" "+ str(playerAge)+" " + " \n"+"press to continue ");
 else:
-    print("you are"+" "+str(playerAge)+" "+"years old\n" + "you are under 18 so just go back")
+    print(playerName +" you are"+" "+str(playerAge)+" "+"years old\n" + "you are under 18 so just go back")
