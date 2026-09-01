e5ddfac2811c0f05afd2e832a057d929f5ff90df
sinajr <jamyarisina@gmail.com>
2026-09-01 20:34:19 +0300

    adding some comments to project

--- peliprojekti_2/README.md
+++ peliprojekti_2/README.md
@@ -1,4 +1,16 @@
-Minipeli
+# Rulettipeli
+
+Tämä on yksinkertainen Pythonilla tehty rulettipeli.
+
+Ohjelma kysyy pelaajan nimen ja iän. Pelaaja lisää rahaa pelitilille, ja yksi veto maksaa 2 euroa. Pelaaja voi veikata:
+
+> numeroa 0–36
+> punaista tai mustaa
+> parillista tai paritonta numeroa
+
+Lopuksi ohjelma arpoo ruletin tuloksen ja näyttää pelaajan valinnat.
+
+## Ohjelman käynnistäminen
 
 Sina Jamyari
 
--- peliprojekti_2/minipeli_rolet.py
+++ peliprojekti_2/minipeli_rolet.py
@@ -3,63 +3,79 @@ import random
 # täälä otettan käyttäjän nimi ja ikä 
 
 
+while True:
 
+    playerName = input("what is the player name? ");
+    playerAge = int(input("player age: "));
 
-playerName = input("what is the player name? ");
-playerAge = int(input("player age: "));
-userChoosedNum=[]
-randomArray=[]
+    #tyhjä lista että säästetään mikä numeroita käytäjä on valinnut
 
-randomNum = random.randint(0,36)
-roulettetiRandom = randomNum
-randomArray.append(roulettetiRandom)
+    userChoosedNum=[]
 
-colorPicker=int(random.random()*100)
-if colorPicker>=50:
-    colorPicker = "red"
-    randomArray.append(colorPicker)
-else:
-    olorPicker = "black"
-    randomArray.append(colorPicker)
+    #tyhjä lista että säästetään ranomit numerot
+    randomArray=[]
 
-evenOrodd=""
-if roulettetiRandom / 2 == 0:
-    evenOrodd="even"
-    randomArray.append(evenOrodd)
-else:
-    evenOrodd="odd"
-    randomArray.append(evenOrodd)
+    # Lasketan randomit numerot
 
+    randomNum = random.randint(0,36)
+    roulettetiRandom = randomNum
+    randomArray.append(roulettetiRandom)
 
-userCredit= 0
-betPrice=2
-if  12 <= playerAge:
-    print("welcome "+ playerName +" "+ "you are"+" "+ str(playerAge)+" ")
-    userCredit=float(input("how much € you are going to bring?\n \nJust enter number: "))
-    betCounter=int(userCredit/betPrice)
-    if userCredit >= 5:
-        print(f"you have {userCredit} € in your wallet \n \nyou can have {betCounter} bet chance")
+    colorPicker=random.random()*100
+    if colorPicker>=50:
+        colorPicker = "red"
+        randomArray.append(colorPicker)
+    else:
+        olorPicker = "black"
+        randomArray.append(colorPicker)
 
-        for i in range(betCounter):
-            userType = input("ok "+playerName+" select your number: \n \nor you can choose black and red \n \nor even or odd numbers \n \nfor betting type bet: ")
-            if userType.isnumeric():
-                if 0 <= int(userType) <= 36:
-                    userChoosedNum.append(userType)
-                else:
-                    print("please enter number between 0 _ 36 or type RED or BLACK, also you can choose EVEN and ODD")
-                    userType= input("try again")
+    evenOrodd=""
+    if roulettetiRandom / 2 == 0:
+        evenOrodd="even"
+        randomArray.append(evenOrodd)
+    else:
+        evenOrodd="odd"
+        randomArray.append(evenOrodd)
+
+    # käytäjän Credit ja joka peli maksu
+    userCredit= 0
+    betPrice=2
+
+    # tarkistetaan käytäjän ikä
+    if  12 <= playerAge:
+        print("welcome "+ playerName +" "+ "you are"+" "+ str(playerAge)+" ")
+
+    #lasketan käytäjän raha ja veto
+        userCredit=float(input("how much € you are going to bring?\n \nJust enter number: "))
+        betCounter=int(userCredit/betPrice)
+
+    #käytäjän credit pitäs olla yli 5 euro
+        if userCredit >= 5:
+            print(f"you have {userCredit} € in your wallet \n \nyou can have {betCounter} bet chance")
+            
+#Varmista, että käyttäjä valitsee kirjoitusarvon ja käyttää sitä panostaakseen niin paljon kuin käyttäjällä on mahdollisuus
+
+            for i in range(betCounter):
+                userType = input("ok "+playerName+" select your numbers: \n \nor you can choose black and red \n \nor even or odd numbers \n \nfor betting type bet: ")
+                if userType.isnumeric():
                     if 0 <= int(userType) <= 36:
                         userChoosedNum.append(userType)
-            else:
-                userChoosedNum.append(userType)
-        i +=1
-        if i == betCounter:
-            print(f"you are out of chance \n \n Your creadit is: {userCredit-(betCounter * betPrice)} € \n \n the result is: ")
-            print(f"this what you choosed {playerName}: \n{userChoosedNum}")
-            print(f"the roulette result is \n{randomArray}")
+                    else:
+                        print("please enter number between 0 _ 36 or type RED or BLACK, also you can choose EVEN and ODD")
+                        userType= input("try again")
+                        if 0 <= int(userType) <= 36:
+                            userChoosedNum.append(userType)
+                else:
+                    userChoosedNum.append(userType)
+            i +=1
+            #ja tulostetaan loppu peli
+            if i == betCounter:
+                print(f"you are out of chance \n \n Your creadit is: {userCredit-(betCounter * betPrice)} € \n \n the result is: ")
+                print(f"this what you choosed {playerName}: \n{userChoosedNum}")
+                print(f"the roulette result is \n{randomArray}")
 
 
+        else:
+            print("you should charge at least 5€ in your wallet")
     else:
-        print("you should charge at least 5€ in your wallet")
-else:
-    print(playerName +" you are"+" "+str(playerAge)+" "+"years old\n" + "you are under 18 so just go back")
+        print(playerName +" you are"+" "+str(playerAge)+" "+"years old\n" + "you are under 18 so just go back")
